b6c2472dcc8269f3d5966ac11bad33abd14e4e66
ABHIKRIT RAJ SINGH <56408635+abhikrit1212@users.noreply.github.com>
2022-02-28 11:28:53 +0530

    reset-password added

--- activitymanagement/src/main/java/com/peoplestrong/activitymanagement/api/PasswordController.java
+++ activitymanagement/src/main/java/com/peoplestrong/activitymanagement/api/PasswordController.java
@@ -8,26 +8,17 @@ import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 
 import com.peoplestrong.activitymanagement.payload.response.MessageResponse;
+import com.peoplestrong.activitymanagement.service.EmailService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import com.sun.net.httpserver.Authenticator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.peoplestrong.activitymanagement.models.User;
-import com.peoplestrong.activitymanagement.service.EmailService;
 import com.peoplestrong.activitymanagement.service.UserService;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -63,38 +54,21 @@ public ResponseEntity<?> processForgotPasswordForm(HttpServletRequest request,@R
         userService.saveUser(user);
 
         String appUrl = request.getScheme() + "://" + request.getServerName();
-
-        // Email message
-        SimpleMailMessage passwordResetEmail = new SimpleMailMessage();
-        passwordResetEmail.setFrom("abhikrit1212@gmail.com");
-        passwordResetEmail.setTo(user.getUsername());
-        passwordResetEmail.setSubject("Password Reset Request");
-        passwordResetEmail.setText("To reset your password, click the link below:\n" + appUrl
-                + "/reset?token=" + user.getResetToken());
-
-        emailService.sendEmail(passwordResetEmail);
+        String to=user.getUsername();
+        String subject="Password Reset Request";
+        String message="To reset your password, click the link below:\n" + appUrl + "/reset?token=" + user.getResetToken();
+        emailService.sendEmail(subject,message,to);
         return ResponseEntity.status(HttpStatus.OK).body("Reset Password link has been set to your email");
     }
 }
-
-    // Process reset password
     @PostMapping("/reset-password")
     public ResponseEntity<?> setNewPassword(@RequestBody Map<String, String> body) {
-
-        // Find the user associated with the reset token
-        System.out.println(body.get("token"));
         Optional<User> user = userService.findUserByResetToken(body.get("token"));
         if (user.isPresent()) {
             URI uri=URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/reset-password").toUriString());
-
             User resetUser = user.get();
-
             resetUser.setPassword(body.get("password"));
-
-            // Set the reset token to null so it cannot be used again
             resetUser.setResetToken(null);
-
-            // Save user
             userService.saveUser(resetUser);
             return new ResponseEntity<Authenticator.Success>(HttpStatus.OK);
         }
